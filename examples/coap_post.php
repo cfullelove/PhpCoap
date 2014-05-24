d97ebaaa1d108450803e66506a51d22b1a0f0797
Chris Fullelove <cfullelove@gmail.com>
2014-05-24 23:58:15 +1000

    Added new example showing use of Stream and socket

--- examples/coap_post.php
+++ examples/coap_post.php
@@ -8,7 +8,7 @@ $client = new PhpCoap\Client( $loop );
 
 $client->post( 'coap://skynet.im/devices', 'type=test', function( $data ) {
 	var_dump( json_decode( $data ) );
-} );
+});
 
 $loop->run();
 
---
+++ examples/stdin_to_coap.php
@@ -0,0 +1,32 @@
+<?php
+
+require( __DIR__ . '/../vendor/autoload.php' );
+
+$loop = new PhpCoap\StreamSocketSelectLoop();
+
+$client = new PhpCoap\Client( $loop );
+
+$client->post( 'coap://skynet.im/devices', 'type=test', function ( $data ) use ($loop, $client) {
+	$data = json_decode( $data );
+	$uuid = $data->uuid;
+	$token = $data->token;
+
+	printf( "UUID: %s\nToken: %s\n", $uuid, $token );
+
+	$stdin = new React\Stream\Stream( fopen( 'php://stdin', 'r' ), $loop );
+
+	$stdin->on( 'data', function ($data) use ($client, $uuid, $token) {
+
+		$query = http_build_query( array(
+			'token' => $token,
+			'value' => trim( $data )
+		));
+
+		$client->post( sprintf( 'coap://skynet.im/data/%s', $uuid ), $query, function( $data ) {
+			var_dump( json_decode( $data ));
+		} );
+	});
+});
+
+
+$loop->run();
